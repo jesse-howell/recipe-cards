92bd0c9504434eb902479e9bc37a852413da064b
sailorshy94 <sbutle94@gmail.com>
2024-02-07 21:20:48 -0500

    removed json file for recipes - teammate created, added recipedata seeding to index.js seeds file, added recipe.js file to models dir, in models index.js added const for recipe model and exported

--- seeds/index.js
+++ seeds/index.js
@@ -1,15 +1,20 @@
 const sequelize = require('../config/connection');
 const { User } = require('../models');
+const { Recipe } = require('../models');
 
 const userData = require('./userData.json');
+const recipeData = require('./recipeData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+  console.log('\n----- DB SEEDED -----');
 
   await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+
+  await Recipe.bulkCreate(recipeData);
 
   process.exit(0);
 };
