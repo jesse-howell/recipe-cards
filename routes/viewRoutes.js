73c989c07fba371c10d7f64048cec078c6c444ef
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2024-02-08 21:06:16 -0500

    made login form, added submit and create account buttons, made createaccount.handlebars

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../models');
 
 router.get('/', async (req, res) => {
-  res.render('index');
+  res.render('homepage');
 });
 
 router.get('/login', (req, res) => {
