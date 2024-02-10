2e06b2c993d39687b4f8e3ce565ead670c9f2041
DaymensCode <Daymenscode@gmail.com>
2024-02-09 23:04:29 -0500

    added temp route for createuser

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -9,6 +9,17 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+//will be for creating a user initially (still needs a .handlebars file created in views)
+router.get('/createUser', (req, res) => {
+
+
+  //placeholder name for now, but this will be name of .handlebars file)
+  res.render('createuser');
+});
+
+
+
+
 router.get('/dashboard', (req, res) => {
   res.render('dashboard');
 });
