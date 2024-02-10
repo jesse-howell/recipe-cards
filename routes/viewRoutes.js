2c14eab917c66ee4ce67f084fff9bea411002e1d
Seven <phoenixpyra7@gmail.com>
2024-02-10 13:12:13 -0600

    added create and delete routes to all routes pages, added find all and find one to recipes, added update to recipes as well. added some comments. Need to address questions at next meet up. not fully completed, good stopping point.

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,12 +1,10 @@
-const router = require('express').Router();
-const { User } = require('../models');
-
+const router = require("express").Router();
+const { User } = require("../models");
 
 //homepage route
-router.get('/', async (req, res) => {
-  res.render('homepage');
+router.get("/", async (req, res) => {
+  res.render("homepage");
 });
-
 
 // login page will be integrated into homepage w/ sep pg for create user
 //login page
@@ -14,27 +12,20 @@ router.get('/', async (req, res) => {
 //   res.render('login');
 // });
 
-
-
 //will be for creating a user initially (still needs a .handlebars file created in views)
-router.get('/createuser', (req, res) => {
-
-
+router.get("/createuser", (req, res) => {
   //placeholder name for now, but this will be name of .handlebars file)
-  res.render('createuser');
+  res.render("createuser");
 });
-
 
 //create recipe page
-router.get('/createrecipe', (req, res) => {
-  res.render('createrecipe');
+router.get("/createrecipe", (req, res) => {
+  res.render("createrecipe");
 });
 
-
-
 //your recipe page
-router.get('/yourrecipes', (req, res) => {
-  res.render('yourrecipes');
+router.get("/yourrecipes", (req, res) => {
+  res.render("yourrecipes");
 });
 
 module.exports = router;
