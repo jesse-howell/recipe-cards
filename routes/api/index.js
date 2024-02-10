dbe5d6d6614e8e18b631cc599e5ef5d0523d77e7
DaymensCode <Daymenscode@gmail.com>
2024-02-10 11:12:56 -0500

    we worked on route  logic for user login,creation, logout

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+
+const userRoutes = require('./ingredientRoutes');
+
+const userRoutes = require('./noteRoutes');
+
+const userRoutes = require('./recipeRoutes');
+
+const userRoutes = require('./userRoutes');
 // create one for each route - recipes, ingredients, instructions, notes
 // const = require('./');
 // const = require('./');
