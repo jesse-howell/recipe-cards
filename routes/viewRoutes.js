4b84bbfcb05b0fc6d9716710bcdf25bc4ca54a95
DaymensCode <Daymenscode@gmail.com>
2024-02-09 23:07:07 -0500

    added recepie landing page routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -10,7 +10,7 @@ router.get('/login', (req, res) => {
 });
 
 //will be for creating a user initially (still needs a .handlebars file created in views)
-router.get('/createUser', (req, res) => {
+router.get('/createuser', (req, res) => {
 
 
   //placeholder name for now, but this will be name of .handlebars file)
@@ -20,8 +20,15 @@ router.get('/createUser', (req, res) => {
 
 
 
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard');
+
+
+router.get('/createrecipe', (req, res) => {
+  res.render('createreceipe');
+});
+
+
+router.get('/yourrecipes', (req, res) => {
+  res.render('yourrecipes');
 });
 
 module.exports = router;
