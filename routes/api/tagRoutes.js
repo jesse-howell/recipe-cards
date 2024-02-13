f9325da6d449f1bd28e22021e944e373f50e845f
DaymensCode <Daymenscode@gmail.com>
2024-02-12 22:44:44 -0500

    opting out of using tags for now, commenting out route

--- routes/api/tagRoutes.js
+++ routes/api/tagRoutes.js
@@ -1,47 +1,47 @@
-const router = require("express").Router();
-const {
-  Recipe,
-  Ingredient,
-  Instruction,
-  Note,
-  Category,
-  Tag,
-} = require("../../models");
+// const router = require("express").Router();
+// const {
+//   Recipe,
+//   Ingredient,
+//   Instruction,
+//   Note,
+//   Category,
+//   Tag,
+// } = require("../../models");
 
-//Push/Post/Delete Options:
+// //Push/Post/Delete Options:
 
-// create new -> tag
-router.post("/", async (req, res) => {
-  try {
-    const tag = await Tag.create(req.body);
-    return res.status(200).json(tag);
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json(err);
-  }
-});
+// // create new -> tag
+// router.post("/", async (req, res) => {
+//   try {
+//     const tag = await Tag.create(req.body);
+//     return res.status(200).json(tag);
+//   } catch (err) {
+//     console.log(err);
+//     return res.status(500).json(err);
+//   }
+// });
 
-// delete -> tag
-// I used the async/await syntax for this block of code
-router.delete("/:id", async (req, res) => {
-  try {
-    const TagData = await Tag.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
+// // delete -> tag
+// // I used the async/await syntax for this block of code
+// router.delete("/:id", async (req, res) => {
+//   try {
+//     const TagData = await Tag.destroy({
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
 
-    if (!TagData) {
-      res.status(404).json({ message: "Tag not found." }); // if user messed up it's 404
-      return;
-    }
+//     if (!TagData) {
+//       res.status(404).json({ message: "Tag not found." }); // if user messed up it's 404
+//       return;
+//     }
 
-    res.status(200).json({ message: "Tag deleted" }); // 200 means all good
-  } catch (err) {
-    if (err) {
-      res.status(500).json(err); // if server messed up its a 500
-    }
-  }
-});
+//     res.status(200).json({ message: "Tag deleted" }); // 200 means all good
+//   } catch (err) {
+//     if (err) {
+//       res.status(500).json(err); // if server messed up its a 500
+//     }
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
