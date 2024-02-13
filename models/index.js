028b27ab5d607c9e4b0045c9d6a324cd32016ef9
DaymensCode <Daymenscode@gmail.com>
2024-02-13 15:01:55 -0500

    trimmed down current index.js in models>index.js to use with new MVP goal

--- models/index.js
+++ models/index.js
@@ -1,22 +1,6 @@
 const User = require("./User");
 const Recipe = require("./Recipe");
-// const Ingredient = require("./Ingredient");
-// const Instruction = require("./Instruction");
-// const Note = require("./Note");
-// doing away w/ Tag for now
-// const Tag = require("./Tag");
 
-// Ingredient belongsTo Recipe
-// Ingredient.belongsTo(Recipe, {
-//   foreignKey: "recipeId",
-//   onDelete: "CASCADE",
-// });
-
-// Instruction belongsTo Recipe
-// Instruction.belongsTo(Recipe, {
-//   foreignKey: "recipeId",
-//   onDelete: "CASCADE",
-// });
 
 // Notes belongsTo Reccipe
 // Note.belongsTo(Recipe, {
@@ -24,39 +8,13 @@ const Recipe = require("./Recipe");
 //   onDelete: "CASCADE",
 // });
 
-// not using Tag 
-// Tag belongsTo Recipe
-// Tag.belongsTo(Recipe, {
-//   foreignKey: "recipeId",
-//   onDelete: "CASCADE",
-// });
-
-// Recipe have many ingredients
-// Recipe.hasMany(Ingredient, {
-//   foreignKey: "recipeId",
-// });
-
-// Recipe have many instructions
-// Recipe.hasMany(Instruction, {
-//   foreignKey: "recipeId",
-// });
-
 // Recipe have many notes
 // Recipe.hasMany(Note, {
 //   foreignKey: "recipeId",
 // });
 
-// doing away with tags - not enough time to complete/too complex
-// Recipe have many tags
-// Recipe.hasMany(Tag, {
-//   foreignKey: "recipeId",
-// });
 
 module.exports = {
   User,
   Recipe,
-  // Ingredient,
-  // Instruction,
-  // Note,
-  // Tag,
 };
