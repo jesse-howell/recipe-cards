8361d64117f3eb5909b3e699146dcbdae9b741e9
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2024-02-13 23:09:14 -0500

    added period to h2 titles, added links to nav bar

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -72,7 +72,7 @@ router.post("/login", async (req, res) => {
 router.post("/logout", (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
-      res.status(204).end();
+      res.status(204).redirect('/login');
     });
   } else {
     res.status(404).end();
